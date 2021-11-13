d6376f77eacf030d59286ae13ecae544d806ffee
vibhorejain09 <vibhorejain0911@gmail.com>
2021-11-13 21:36:33 +0530

    UIbuild

--- src/Components/VerifyEmail/VerifyEmail.js
+++ src/Components/VerifyEmail/VerifyEmail.js
@@ -8,7 +8,7 @@ function VerifyEmail() {
   const [severity, setSeverity] = useState("")
   const [message, setMessage] = useState(null)
   const { currentUser } = useAuth()
-  console.log(currentUser)
+
   async function handleSubmit() {
     setLoading(true)
     setMessage(null)
@@ -17,28 +17,29 @@ function VerifyEmail() {
       .then(() => {
         setSeverity("success")
         setMessage("Verification Email Sent!!")
-        
-        setLoading(false)
       })
       .catch((error) => {
         setSeverity("error")
         setMessage("Something went wrong. Please try again..")
-        
-        setLoading(false)
         console.log(error)
       })
+    setLoading(false)
   }
   return (
-    <div id="verify-email"> 
-    <Box sx={{ pt: 12, display: "flex", justifyContent: "center", minHeight:"100vh" }}>
-      <Box sx={{ maxWidth: "800px" }}>
-        {message && <Alert severity={severity}>{message}</Alert>}
-        <Typography component="h2" variant="h5" sx={{ textAlign: "center" }} style={{color: "rgb(var(--green-color))", fontWeight: "bold"}}>Please verify your account to continue</Typography>
-        <Button variant="contained" fullWidth disabled={loading} onClick={handleSubmit} sx={{ mt: 2, mb: 2 }} style={{backgroundColor: "rgb(var(--green-color))"}}>
-           Send Email
-        </Button>
-      </Box>
-    </Box>
+    <div id="verify-email">
+      {
+
+        currentUser ?
+          <Box sx={{ pt: 12, display: "flex", justifyContent: "center", minHeight: "100vh" }}>
+            <Box sx={{ maxWidth: "800px" }}>
+              {message && <Alert severity={severity}>{message}</Alert>}
+              <Typography component="h2" variant="h5" sx={{ textAlign: "center" }} style={{ color: "rgb(var(--green-color))", fontWeight: "bold" }}>Please verify your account to continue</Typography>
+              <Button variant="contained" fullWidth disabled={loading} onClick={handleSubmit} sx={{ mt: 2, mb: 2 }} style={{ backgroundColor: "rgb(var(--green-color))" }}>
+                Send Email
+              </Button>
+            </Box>
+          </Box> : " "
+      }
     </div>
   )
 }
